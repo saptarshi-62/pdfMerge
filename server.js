fa9fd2c0d4cee8c5f94b75c366f5b07cfaafd368
saptarshi-62 <saptarshidet6290186223@gmail.com>
2025-12-30 01:04:48 +0530

    modify

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post(
 */
 
 if (require.main === module) {
-  const port = process.env.PORT || 3000;
+  const port =  3000;
   app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
   });
